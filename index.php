bfd52345df3c3c4251308b470f963580ae702171
TheLuke30 <champion20011@gmail.com>
2020-10-04 13:26:30 -0400

    Added Basic CSS to Index, more to be edited and added later

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
     <!-- CSS only -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <!-- JS, Popper.js, and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -14,10 +14,8 @@
   </head>
 
   <body>
-    <a href="#">Login</a>
-    <a href="student_home.php">Student Home</a>
-      
-    <div class="container">
+    <button class="button" onclick="window.location.href = 'auth.php';"> Login </button>
+    <div>
       <img src="assets/img/logo.png" alt="CourseMap Logo">
       <h1>Why Wait?</h1>
       <p>WRITE DESCRIPTION</p>
