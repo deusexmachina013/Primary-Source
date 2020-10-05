67da23777dc3dcc807daf578bb42132733c1dbe5
Joyce Fang <joycefang01@gmail.com>
2020-10-05 18:13:46 -0400

    Created student_navbar.php and included it in all student pages. Cleaned up the student file names (i.e. changed from student_home.php to home.php)

--- student/all_plans.php
+++ student/all_plans.php
@@ -2,11 +2,11 @@
 
 <html lang="en">
   <head>
-    <title>Student All Plans</title>>
+    <title>Student All Plans</title>
 
     <!-- CSS ONLY -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <!-- JS, Popper.js, and jQuery -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -15,30 +15,7 @@
 
   <body>
     <!--NAVIGATION BAR (implemented with Bootstrap) -->
-    <nav class="navbar navbar-custom navbar-expand-lg navbar-light fixed-top">
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarNav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger active" href="student_home.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="student_create_plan.php">Create Plan</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="all_plans.php">All Plans</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="student_catalog.php">Catalog</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="index.php">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <?php include('student_navbar.php'); ?>
 
     <section class="content">
       <!-- All Plans -->
---
+++ student/student_navbar.php
@@ -0,0 +1,25 @@
+<!--NAVIGATION BAR (implemented with Bootstrap) -->
+<nav class="navbar navbar-custom navbar-expand-lg navbar-light fixed-top">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger active" href="/student/home.php">Home</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="/student/create_plan.php">Create Plan</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="/student/all_plans.php">All Plans</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="/student/catalog.php">Catalog</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link js-scroll-trigger" href="../index.php">Logout</a>
+        </li>
+    </ul>
+    </div>
+</nav>
