fcff57bd7270740a85b3a373f9e56d2d3a0c7fa2
anya-tralshawala <55996101+anya-tralshawala@users.noreply.github.com>
2020-10-13 16:59:01 -0400

    Create test.php

---
+++ student/all/test.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <title>Test Plan</title>
+
+    <!-- CSS ONLY -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <!--NAVIGATION BAR (implemented with Bootstrap) -->
+    <?php include('../student_navbar.php'); ?>
+
+    <section class="content">
+      <!-- MAIN PLAN -->
+      <h1>Test Plan</h1>
+      <!-- YEAR 1 -->
+      <div class="container">
+        <h2>First Year</h2>
+        <!-- ROW 1 -->
+        <div class="row">
+          <div class="col-sm col-semester">
+            Semester I
+          </div>
+          <div class="col-sm col-semester">
+            Semester II
+          </div>
+        </div>
+        <!-- ROW 2 -->
+        <div class="row">
+          <div class="col-sm">
+            Introduction to ITWS
+          </div>
+          <div class="col-sm">
+            Financial Accounting
+          </div>
+        </div>
+        <!-- ROW 3 -->
+        <div class="row">
+          <div class="col-sm">
+            Computer Science 1
+          </div>
+          <div class="col-sm">
+            Data Structures
+          </div>
+        </div>
+        <!-- ROW 4 -->
+        <div class="row">
+          <div class="col-sm">
+            Introduction to Management
+         </div>
+         <div class="col-sm">
+            IT and Society
+        </div>
+      </div>
+      <!-- ROW 5 -->
+      <div class="row">
+        <div class="col-sm">
+            Calculus II
+        </div>
+        <div class="col-sm">
+            General Physics
+        </div>
+      </div>
+
+      <!-- ROW 6 -->
+      <div class="row">
+        <div class="col-sm">
+
+        </div>
+        <div class="col-sm">
+          
+        </div>
+
+      </div>
+    </div>
+
+    <!-- YEAR 2 -->
+    <div class="container">
+      <h2>Second Year</h2>
+      <!-- ROW 1 -->
+      <div class="row">
+        <div class="col-sm col-semester">
+          Fall 2020
+        </div>
+        <div class="col-sm col-semester">
+          Spring 2021
+        </div>
+      </div>
+      <!-- ROW 2 -->
+      <div class="row">
+        <div class="col-sm">
+          Foundations of Computer Science
+        </div>
+        <div class="col-sm">
+          Introduction to Algorithms
+        </div>
+      </div>
+      <!-- ROW 3 -->
+      <div class="row">
+        <div class="col-sm">
+         Computer Organization
+       </div>
+       <div class="col-sm">
+        Mathematical Models in Management and Economics
+       </div>
+     </div>
+     <!-- ROW 4 -->
+     <div class="row">
+      <div class="col-sm">
+        Web Systems Development
+      </div>
+      <div class="col-sm">
+       Web Science Systems Development
+     </div>
+   </div>
+   <!-- ROW 5 -->
+   <div class="row">
+    <div class="col-sm">
+      Marketing Principles
+    </div>
+    <div class="col-sm">
+     Operations Management
+    </div>
+  </div>
+
+  <!-- ROW 6 -->
+  <div class="row">
+    <div class="col-sm">
+    </div>
+    <div class="col-sm">
+     Principles of Economics
+    </div>
+
+  </div>
+  </div>
+
+  <!-- YEAR 3 -->
+  <div class="container">
+    <h2>Third Year</h2>
+    <!-- ROW 1 -->
+    <div class="row">
+      <div class="col-sm col-semester">
+        Arch Summer 2021
+      </div>
+      <div class="col-sm col-semester">
+        Fall 2021/Spring 2022
+      </div>
+    </div>
+    <!-- ROW 2 -->
+    <div class="row">
+      <div class="col-sm">
+      Managerial Finance
+      </div>
+      <div class="col-sm">
+      Statistical Methods
+      </div>
+    </div>
+    <!-- ROW 3 -->
+    <div class="row">
+      <div class="col-sm">
+      Organizational Behavior in High Performance Organizations
+      </div>
+      <div class="col-sm">
+        Managing IT Resources
+      </div>
+    </div>
+    <!-- ROW 4 -->
+    <div class="row">
+      <div class="col-sm">
+      Introduction to Biology
+      </div>
+      <div class="col-sm">
+        Database Systems
+      </div>
+    </div>
+    <!-- ROW 5 -->
+    <div class="row">
+      <div class="col-sm">
+      </div>
+      <div class="col-sm">
+      Human Resources in High Perf. Organizations
+      </div>
+    </div>
+
+    <!-- ROW 6 -->
+    <div class="row">
+      <div class="col-sm">
+      </div>
+      <div class="col-sm">
+      Business Law and Ethics
+      </div>
+    </div>
+  </div>
+
+  <!-- YEAR 4 -->
+  <div class="container">
+    <h2>Fourth Year</h2>
+    <!-- ROW 1 -->
+    <div class="row">
+      <div class="col-sm col-semester">
+        Fall 2022
+      </div>
+      <div class="col-sm col-semester">
+        Spring 2023
+      </div>
+    </div>
+    <!-- ROW 2 -->
+    <div class="row">
+      <div class="col-sm">
+      Quantitative Methods for Business
+      </div>
+      <div class="col-sm">
+      Data Resource Management
+      </div>
+    </div>
+    <!-- ROW 3 -->
+    <div class="row">
+      <div class="col-sm">
+      "Stategy and Policy"
+      </div>
+      <div class="col-sm">
+      Information Technology and Web Science Capstone
+      </div>
+    </div>
+    <!-- ROW 4 -->
+    <div class="row">
+      <div class="col-sm">
+      Enterprise IT Integration
+      </div>
+      <div class="col-sm">
+      Intro to Human Computer Interaction
+      </div>
+    </div>
+    <!-- ROW 5 -->
+    <div class="row">
+      <div class="col-sm">
+        System Analysis and Design
+      </div>
+      <div class="col-sm">
+      IT Project Management
+      </div>
+    </div>
+
+    <!-- ROW 6 -->
+    <div class="row">
+      <div class="col-sm">
+
+      </div>
+      <div class="col-sm">
+     </div>
+
+   </div>
+  </div>
+
+  </section>
+  </body>
+</html>
