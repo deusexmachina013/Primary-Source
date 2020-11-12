85688d1133d2306d61ef936139e32ef8565206cd
Jacob Dyer <dyerj@rpi.edu>
2020-11-12 14:56:41 -0500

    Generalized Searchbar Functionality
    Generalized the searchbar functionality and made it so that the code can be extended out to other documents, and provided that the HTML is in place, work without ever needing to touch the JavaScript.
    
    Co-Authored-By: Luke <55993047+Luke-El@users.noreply.github.com>

--- admin/home.php
+++ admin/home.php
@@ -9,10 +9,11 @@
     <link rel="stylesheet" href="../assets/css/style.css">
 
     <!-- JS, Popper.js, and jQuery -->
-    <script src="../assets/js/admin_homepage.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <script src="../assets/js/admin_homepage.js"></script>
   </head>
 
   <body>
@@ -21,7 +22,7 @@
     <main class="content">
       <h1>Your Students' Plans</h1>
       <div class="search-bar">
-        <input type="text" class="admin-loopup" placeholder="Search for student plans..." id="admin-lookup" onkeyup="filterStudentPlans()">
+        <input type="text" class="student-searchbar" placeholder="Search for student plans..." id="admin-lookup">
       </div>
       <table class="center" id="plan-table">
         <!-- Table Heading -->
@@ -38,19 +39,19 @@
         </div>
         <!-- Table Body -->
         <tbody>
-          <tr>
+          <tr class="student">
             <td class="name">Jacob Dyer</td>
             <td class="year">2023</td>
             <td class="status">Reviewable</td>
             <td class="date">10/4/2020</td>
           </tr>
-          <tr>
+          <tr class="student">
             <td class="name">Joyce Fang</td>
             <td class="year">2023</td>
             <td class="status">Approved</td>
             <td class="date">10/4/2020</td>
           </tr>
-          <tr>
+          <tr class="student">
             <td class="name">Bobby Tables</td>
             <td class="year">2024</td>
             <td class="status">None</td>
