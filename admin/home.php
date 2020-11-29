f73f50496df870eed3f036c712d3b707cef9279f
TheLuke30 <champion20011@gmail.com>
2020-11-28 19:46:05 -0500

    Started Updating Table

--- admin/home.php
+++ admin/home.php
@@ -20,12 +20,12 @@
 
     <main class="content">
       <h1>Your Students' Plans</h1>
-<<<<<<< Updated upstream
+
       <div class="search-bar">
         <input type="text" class="admin-loopup" placeholder="Search for student plans..." id="admin-lookup" onkeyup="filterStudentPlans()">
       </div>
       <table class="center" id="plan-table">
-=======
+
       <form class="student-search center" accept-charset="utf-8" method="GET" name="student-search" role="search">
         <div class="left-partition">
           <select id="student-search-dropdown" class="search-dropdown" title="Filter by">
@@ -42,37 +42,37 @@
       </form>
 
       <table class="table table-bordered table-striped table-hover center" id="plan-table">
->>>>>>> Stashed changes
+
         <!-- Table Heading -->
         <div class = "col-sm col-semester">
         <thead class="thead-light">
           <tr>
-            <th scope="col" class="student-column">Student</th>
-            <th scope="col" class="year-column">Class Year</th>
-            <th scope="col" class="status-column">Status</th>
-            <th scope="col" class="date-column">Submission Date</th>
+            <th scope="col">Student</th>
+            <th scope="col">Class Year</th>
+            <th scope="col">Status</th>
+            <th scope="col">Submission Date</th>
           </tr>
         </thead>
         </div>
         <!-- Table Body -->
         <tbody>
-          <tr>
-            <td class="name">Jacob Dyer</td>
-            <td class="year">2023</td>
-            <td class="status">Reviewable</td>
-            <td class="date">10/4/2020</td>
+          <tr scope="row">
+            <td>Jacob Dyer</td>
+            <td>2023</td>
+            <td>Reviewable</td>
+            <td>10/4/2020</td>
           </tr>
-          <tr>
-            <td class="name">Joyce Fang</td>
-            <td class="year">2023</td>
-            <td class="status">Approved</td>
-            <td class="date">10/4/2020</td>
+          <tr scope="row">
+            <td>Joyce Fang</td>
+            <td>2023</td>
+            <td>Approved</td>
+            <td>10/4/2020</td>
           </tr>
-          <tr>
-            <td class="name">Bobby Tables</td>
-            <td class="year">2024</td>
-            <td class="status">None</td>
-            <td class="date">N/A</td>
+          <tr scope="row">
+            <td>Bobby Tables</td>
+            <td>2024</td>
+            <td>None</td>
+            <td>N/A</td>
           </tr>
         </tbody>
       </table>
